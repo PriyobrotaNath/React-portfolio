d1458172d6cfd17dbe28ec0812b52382361c4330
Priyobrota Nath <pnath161256@bscse.uiu.ac.bd.com>
2022-02-26 02:52:05 +0600

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar.js";
+
 
 function App() {
-    return ( <
-        h2 > protfolio Project... < /h2>
+    return ( 
+        <Navbar/>
     );
 }
 
